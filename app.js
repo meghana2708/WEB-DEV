6f7bed94d9babaa6b9cde037064f2a8055913058
Sai Meghana Vemula <meghana.bamr@gmail.com>
2021-09-28 18:56:11 +0530

    data types

---
+++ app.js
@@ -0,0 +1,12 @@
+//string
+console.log("hello world")
+console.warn("hello warning")
+console.error("hello error")
+console.log(typeof "d")
+
+//number
+console.log(1)
+
+//boolean
+console.log(typeof true)
+console.log(false)
