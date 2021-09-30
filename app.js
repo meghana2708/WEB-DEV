cd446b40271367d2ded58f95e6335558428b7b2b
Sai Meghana Vemula <meghana.bamr@gmail.com>
2021-09-30 23:06:32 +0530

    my coomit

--- app.js
+++ app.js
@@ -1,34 +1,6 @@
-var a = 2
-var b = 10
-console.log('a+b =', a + b)
-console.log('b-a =', b - a)
-console.log('b**a =', b ** a)
-
-//asssigment operators
-console.log('b', b)
-b += 10
-console.log('b+=', b)
-b++
-console.log('b++', b)
-
-//comparision operators
-var d = 10
-var e = 2
-var f = '10'
-
-console.log(d > e)
-console.log(d < e)
-console.warn(d == f)
-console.warn(d + f)
-console.warn(d === f)
-
-//logical operators
-console.log(true && true)
-console.log(true && false)
-console.log(false && false)
-console.log(false && true)
-
-console.log(true || true)
-console.log(true || false)
-console.log(false || true)
-console.log(false || false)
+var headig = document.getElementById('heading')
+var para = document.getElementsByClassName('para')
+headig.innerHTML = "hi from js"
+console.log(headig)
+console.log(para)
+console.log(para[1])
