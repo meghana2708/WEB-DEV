71ddfcb81a8c9eb7cff29df561716b6bcdef75fc
Sai Meghana Vemula <meghana.bamr@gmail.com>
2021-10-01 08:30:11 +0530

    push

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var list = document.querySelector('ul')
 console.log(list)
 var arr = ['go to gym', 'cook', 'eat']
+arr.push('complete')
 for (var i = 0; i < arr.length; i++) {
     var element = document.createElement('li')
     var textNode = document.createTextNode(arr[i])
         //method to create textnode
     element.appendChild(textNode)
     console.log(element)
+    list.appendChild(element)
 
 }
