ca66a187b3ebeb25a4fa9303ac9a9c7355a64d40
Sai Meghana Vemula <meghana.bamr@gmail.com>
2021-10-01 08:04:47 +0530

    dark

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var headig = document.getElementById('heading')
-var para = document.getElementsByClassName('para')
-var tags = document.getElementsByTagName('a')
-var query = document.querySelector('#heading')
-
-console.log(query)
+var body = document.querySelector('body')
+headig.inerHTML = "i am ot a headig"
+headig.style = "color:red;font-size:2rem"
+console.log(headig.classList)
+    //gives the list of classes
+headig.classList.add('five')
+headig.classList.remove('one')
+body.classList.add('dark')
