05aa1980273fbd8c3928c7138888a2fa92fc0567
Sai Meghana Vemula <meghana.bamr@gmail.com>
2021-10-01 07:42:51 +0530

    querysesSelector

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var headig = document.getElementById('heading')
 var para = document.getElementsByClassName('para')
-headig.innerHTML = "hi from js"
-console.log(headig)
-console.log(para)
-console.log(para[1])
-para.push('asdf')
+var tags = document.getElementsByTagName('a')
+var query = document.querySelector('#heading')
+
+console.log(query)
