7bf28892d1811c8571af42bf0c29dabf97dbac7c
Sai Meghana Vemula <meghana.bamr@gmail.com>
2021-10-02 14:52:04 +0530

    event listener

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-//Call back function: executred after a specific task is completed
-var arr = ['go to gym', 'eat food', 'clean house']
+var button = document.querySelector('button')
+var input = document.querySelector('input')
+var list = document.querySelector('ul')
 
 
-var callbackFunction = (element, any) => {
-    console.log(element, any)
+const callbackfunc = () => {
+    console.log('sdf')
 }
-arr.forEach((element, index) => {
-    console.log(element, index)
-})
+
+button.addEventListener('click', callbackfunc)
+
+const callback = (event) => {
+    console.log(event)
+}
+
+button.addEventListener('click', callback)
+
+const call = (event) => {
+    console.log(event.target)
+    console.log(input.value)
+}
+
+button.addEventListener('click', call)
+
+const cal = (event) => {
+    const inputValue = input.value
+    const element = document.createElement('li')
+    const textNode = document.createTextNode(inputValue)
+    element.appendChild(textNode)
+    list.appendChild(element)
+}
+
+button.addEventListener('click', cal)
