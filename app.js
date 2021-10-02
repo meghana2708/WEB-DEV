c021da2c8ca71c6d424206f5d7eee1326be71378
Sai Meghana Vemula <meghana.bamr@gmail.com>
2021-10-02 12:46:53 +0530

    functions

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
-var list = document.querySelector('ul')
-console.log(list)
-var arr = ['go to gym', 'cook', 'eat']
-arr.push('complete')
-for (var i = 0; i < arr.length; i++) {
-    var element = document.createElement('li')
-    var textNode = document.createTextNode(arr[i])
-        //method to create textnode
-    element.appendChild(textNode)
-    console.log(element)
-    list.appendChild(element)
+//function expression
+var greet = function(Name, age) {
+    console.log(Name, age)
+
+
 
 }
+greet('john', 99)
+
+
+
+
+//function keyword
+function add(a, b) {
+    var sum = a + b
+    return sum
+}
+var result = add(10, 20)
+console.log(result)
+
+
+//function expression
+var add = function add(a, b) {
+    var sum = a + b
+    return sum
+}
+
+
+console.log(add(10, 20))
+
+//this is a error because add is not declared before
+//console.log(add(10, 20))
+/*var add = function add(a, b) {
+    var sum = a + b
+    return sum
+}*/
+
+//arrow function
+var add = (a = 0, b = 0) => {
+    var sum = a + b
+    return sum
+}
+console.log(add(10, 20))
+
+//shortcut
+var add = (a = 0, b = 0) => a + b
+console.log(add(30, 40))
+
+//
+const greeting = name => 'hi' + name
+
+console.log(greeting('meghana'))
