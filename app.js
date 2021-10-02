7fd89a03fea4f32f61ebec52eef4e27eae3444b9
Sai Meghana Vemula <meghana.bamr@gmail.com>
2021-10-02 16:39:46 +0530

    toggle

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
 var button = document.querySelector('button')
-var input = document.querySelector('input')
-var list = document.querySelector('ul')
+    /*var input = document.querySelector('input')
+    var list = document.querySelector('ul')*/
+var body = document.querySelector('body')
 
 
-const callbackfunc = () => {
-    console.log('sdf')
-}
+////const callbackfunc = () => {
+////console.log('sdf')
+//}
 
-button.addEventListener('click', callbackfunc)
+////button.addEventListener('click', callbackfunc)
+
+//const callback = (event) => {
+// console.log(event)
+//}
+
+////button.addEventListener('click', callback)
+
+//const call = (event) => {
+// console.log(event.target)
+// console.log(input.value)
+//}
+
+//button.addEventListener('click', call)
+
+//const cal = (event) => {
+//const inputValue = input.value
+//const element = document.createElement('li')
+//const textNode = document.createTextNode(inputValue)
+// element.appendChild(textNode)
+//list.appendChild(element)
+//}
+
+//button.addEventListener('click', cal)
 
 const callback = (event) => {
-    console.log(event)
+    body.classList.toggle('dark')
 }
 
 button.addEventListener('click', callback)
-
-const call = (event) => {
-    console.log(event.target)
-    console.log(input.value)
-}
-
-button.addEventListener('click', call)
-
-const cal = (event) => {
-    const inputValue = input.value
-    const element = document.createElement('li')
-    const textNode = document.createTextNode(inputValue)
-    element.appendChild(textNode)
-    list.appendChild(element)
-}
-
-button.addEventListener('click', cal)
