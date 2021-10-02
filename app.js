99f38271ffe0e8eec877243b0964b643f71f4f15
Sai Meghana Vemula <meghana.bamr@gmail.com>
2021-10-02 13:57:05 +0530

    callbackFunction

--- app.js
+++ app.js
@@ -1,52 +1,10 @@
-//function expression
-var greet = function(Name, age) {
-    console.log(Name, age)
+//Call back function: executred after a specific task is completed
+var arr = ['go to gym', 'eat food', 'clean house']
 
 
-
+var callbackFunction = (element, any) => {
+    console.log(element, any)
 }
-greet('john', 99)
-
-
-
-
-//function keyword
-function add(a, b) {
-    var sum = a + b
-    return sum
-}
-var result = add(10, 20)
-console.log(result)
-
-
-//function expression
-var add = function add(a, b) {
-    var sum = a + b
-    return sum
-}
-
-
-console.log(add(10, 20))
-
-//this is a error because add is not declared before
-//console.log(add(10, 20))
-/*var add = function add(a, b) {
-    var sum = a + b
-    return sum
-}*/
-
-//arrow function
-var add = (a = 0, b = 0) => {
-    var sum = a + b
-    return sum
-}
-console.log(add(10, 20))
-
-//shortcut
-var add = (a = 0, b = 0) => a + b
-console.log(add(30, 40))
-
-//
-const greeting = name => 'hi' + name
-
-console.log(greeting('meghana'))
+arr.forEach((element, index) => {
+    console.log(element, index)
+})
