99dbe5cde5f524221a58f11c8910f65284356c39
Sai Meghana Vemula <meghana.bamr@gmail.com>
2021-10-09 20:33:26 +0530

    json

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-let value = new Promise((re, reject) => {
+/* let value = new Promise((re, reject) => {
     setTimeout(() => {
         re('hello')
     }, 3000)
 })
 setTimeout(() => {
     console.log(value)
-}, 4000)
+}, 4000) */
+
+let response = fetch('https://jsonplaceholder.typicode.com/users').then(res => {
+
+    //.then returns a promise here
+    let parseData = res.json()
+    console.log(parseData)
+    return (parseData)
+}).then(data => { console.log(data) })
