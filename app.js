5e1b721e8f634fd2c0be87918e2b386b96325366
Sai Meghana Vemula <meghana.bamr@gmail.com>
2021-10-10 13:06:35 +0530

    remove elements

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-/* let value = new Promise((re, reject) => {
-    setTimeout(() => {
-        re('hello')
-    }, 3000)
+// /* let response = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(data => {
+//     console.log(data)
+// }) */
+const list = document.querySelector('ul')
+
+let arr = ['one', 'two', 'three', 'four']
+console.log(list)
+arr.forEach(chore => {
+    const element = document.createElement('li')
+    const textNode = document.createTextNode(chore)
+    element.appendChild(textNode)
+    console.log(element)
+    list.appendChild(element)
+    element.addEventListener('click', (e) => {
+        const useInput = window.prompt(`are you sure that you want to delete "${chore}"`)
+        if (useInput == 'YES') e.target.remove()
+    })
 })
-setTimeout(() => {
-    console.log(value)
-}, 4000) */
-
-let response = fetch('https://jsonplaceholder.typicode.com/users').then(res => {
-
-    //.then returns a promise here
-    let parseData = res.json()
-    console.log(parseData)
-    return (parseData)
-}).then(data => { console.log(data) })
