050414d1449e6293bb86f1a5e38f851b80e721d1
Sai Meghana Vemula <meghana.bamr@gmail.com>
2021-10-13 20:08:44 +0530

    private rte route

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
  const express = require("express")
  const PORT = 3000
  const app = express()
- const isAdmin = (req, res, next) => {
-         if (req.headers.admin === 'true') next()
-         else res.send('UNAUTHORISED')
+     /* const isAdmin = (req, res, next) => {
+             if (req.headers.admin === 'true') next()
+             else res.send('UNAUTHORISED')
 
-     }
+         } */
      //  const verify = (req, res, next) => {
      //      //next moves the request to callback
      //      //  app.get('/', (req, res) => {
@@ -13,14 +13,14 @@
      //      if (req.headers['user-agent'] === "Thunder Client (https://www.thunderclient.io)") next() //res.send("GET")
      //      else res.send('BLOCKED')
      //}
- app.get('/public', (req, res) => {
-     console.log(req.headers)
-     res.send('i am a public route')
- })
- app.get('/private', isAdmin, (req, res) => {
+     /* app.get('/public', (req, res) => {
          console.log(req.headers)
-         res.send('i am a admin route')
+         res.send('i am a public route')
      })
+     app.get('/private', isAdmin, (req, res) => {
+             console.log(req.headers)
+             res.send('i am a admin route') */
+     // })
      //  app.post('/', (req, res) => {
      //      res.send('POST')
      //  })
@@ -33,6 +33,10 @@
      //  app.delete('/', (req, res) => {
      //      res.send('DELETE')
      //  })
+ app.post('/signup', (req, res) => {
+     console.log(req.body)
+     res.send('dada')
+ })
  app.listen(PORT, () => {
      console.log(`Server running at ${PORT}`)
  })
