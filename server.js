9c76333ffda3bab4000f2af7eeebe90c92b2d413
Sai Meghana Vemula <meghana.bamr@gmail.com>
2021-10-14 20:05:27 +0530

    authorisation

--- server.js
+++ server.js
@@ -1,43 +1,64 @@
- const express = require("express")
- const PORT = 3000
+ const express = require('express')
  const app = express()
-     /* const isAdmin = (req, res, next) => {
-             if (req.headers.admin === 'true') next()
-             else res.send('UNAUTHORISED')
-
-         } */
-     //  const verify = (req, res, next) => {
-     //      //next moves the request to callback
-     //      //  app.get('/', (req, res) => {
-     //      // console.log(req)
-     //      if (req.headers['user-agent'] === "Thunder Client (https://www.thunderclient.io)") next() //res.send("GET")
-     //      else res.send('BLOCKED')
-     //}
-     /* app.get('/public', (req, res) => {
-         console.log(req.headers)
-         res.send('i am a public route')
-     })
-     app.get('/private', isAdmin, (req, res) => {
-             console.log(req.headers)
-             res.send('i am a admin route') */
-     // })
-     //  app.post('/', (req, res) => {
-     //      res.send('POST')
-     //  })
-     //  app.put('/', (req, res) => {
-     //      res.send('PUT')
-     //  })
-     //  app.patch('/', (req, res) => {
-     //      res.send('PATCH')
-     //  })
-     //  app.delete('/', (req, res) => {
-     //      res.send('DELETE')
-     //  })
+ const PORT = 3000
+ const longer = (req, res, next) => {
+         console.log(req.method)
+         next() //this tells wat kind of request is made
+     }
+     //app.use(longer)//this will be added to each and every request
  app.use(express.json())
- app.post('/signup', (req, res) => {
-     console.log(req.body)
-     res.send('dada')
+ const token = "TOP_SECRET"
+     /* app.get('/', (req, res) => {
+         res.send('Server running')
+     })
+     app.post('/', (req, res) => {
+         console.log(req.body)
+             //express.json is used to convert string to json object
+         res.send('Server running')
+     }) */
+ let products = [{ name: 'iPhone12 Case', price: '999' }, { name: 'iPhone13 Case', price: '1199' }, { name: 'iPhone13 Pro Case', price: '1499' }]
+ const validator = (req, res, next) => {
+         const { name, price } = req.body
+         if (!name || !price) res.json({ error: "Validation failed" })
+         else next()
+     }
+     //-------PUBLIC routes-------------
+ const isAdmin = (req, res, next) => {
+     // const { token }
+     if (req.headers.authorisation === token) {
+
+         next()
+     } else {
+         res.json({ error: "UNAUTHORISED" })
+     }
+
+ }
+ app.get('/products', (req, res) => {
+         res.json({ products })
+     })
+     //----------PRIVATE routes---------------
+ app.post('/products/add', isAdmin, validator, (req, res) => {
+         const { name, price } = req.body
+
+
+         products.push({
+             name,
+             price,
+
+         })
+         res.send({ products })
+
+     })
+     //------authentication
+ app.post('/auth', (req, res) => {
+     const { email, password } = req.body
+     if (email == 'meghana.bamr@gmail.com' && password == 'password') {
+         res.send({ token })
+     } else {
+         res.send({ message: "UNAUTHORISED" })
+     }
  })
+
  app.listen(PORT, () => {
-     console.log(`Server running at ${PORT}`)
+     console.log(`server running at ${PORT}`)
  })
